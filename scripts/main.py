296b76138c5399862250b1364ffa439aacff3204
d3xt3r0 <prathapss87@gmail.com>
2022-02-03 18:59:42 +0530

    Add project name validation

--- scripts/main.py
+++ scripts/main.py
@@ -1,3 +1,6 @@
+from tabnanny import check
+
+
 def init():
     '''
     Collect information from the user
@@ -10,12 +13,29 @@ def init():
     project_author = input(" > Who is the author of this project?\n")
     project_desc = input(" > How do you describe this project?\n")
 
-    #Print the detatils back to the user
+    #Check if the project name contains illegal characters
+    project_name = check_project_name(project_name)
 
+
+    #Print the detatils back to the user
     print("### PROJECT DETAILS ###")
     print(f"Project Name : {project_name}")
     print(f"Author : {project_author}")
     print(f"Project Description : {project_desc}")
+
+
+def check_project_name(name):
+
+    for char in name:
+        if not char.isalnum():
+            if char == '-':
+                continue
+            else:
+                print(f" > The character {char} is not supported")
+                print(" > Please enter new project name")
+                name = check_project_name(input())
+                break
+    return name
 
 
 
